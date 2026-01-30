78bc5794602ba148e02f3dc6e6859c1fd3542468
Jen <tuh42082@temple.edu>
2026-01-30 12:18:28 -0500

    Updates for newer ansible version (#55)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_passenger_package(host):
-    package = host.package('mod_passenger')
-    assert package.is_installed
+def test_passenger_config_present(host):
+    f = host.file("/etc/httpd/conf.modules.d/00-passenger.conf")
+    assert f.exists
+    assert f.contains("LoadModule passenger_module")
